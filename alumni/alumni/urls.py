79bf623a00b29f68d6a40546d5470b3407ea77b8
Angelika2707 <anzelika272011@gmail.com>
2024-06-13 19:10:44 +0300

    Make navbar and create base html

--- alumni/alumni/urls.py
+++ alumni/alumni/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("market/", TemplateView.as_view(template_name="market.html")),
 ]
