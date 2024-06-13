e790280b2de46d653cafc6b5551b92bcfdda29d5
Angelika2707 <anzelika272011@gmail.com>
2024-06-13 19:10:44 +0300

    Make navbar and create base html

--- alumni/alumni/settings.py
+++ alumni/alumni/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'alumni.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static/",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
