1ce9a312a884a086837bed81192da3f166e43e3e
WellNotWell <olesya.gredyushko4956@gmail.com>
2024-06-21 21:00:46 +0300

    add settings for SMTP

--- alumni/alumni/settings.py
+++ alumni/alumni/settings.py
@@ -130,3 +130,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = ''
+EMAIL_PORT = 000
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_TO = ''
