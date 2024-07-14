9d9d43e093922a329a55d79c4a71d1e8a3fe52c2
Angelika2707 <anzelika272011@gmail.com>
2024-07-14 23:01:02 +0300

    fix bugs

--- create_superuser.py
+++ create_superuser.py
@@ -1,9 +1,4 @@
-import os
-import django
 from django.contrib.auth import get_user_model
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alumni.alumni.settings")
-django.setup()
 
 User = get_user_model()
 
