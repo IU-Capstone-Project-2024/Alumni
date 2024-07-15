2312d60637d2387dbbb232a99494ee3997edfd0b
Angelika2707 <anzelika272011@gmail.com>
2024-07-15 16:16:56 +0300

    fix bugs

--- create_superuser.py
+++ create_superuser.py
@@ -1,14 +1,9 @@
-# import os
-# import django
-# from django.contrib.auth import get_user_model
-#
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alumni.alumni.settings")
-# django.setup()
-#
-# User = get_user_model()
-#
-# User.objects.create_superuser(email="student@yandex.ru", password="admin", first_name="Ivan", last_name="Pupkin")
+import os
+import django
 from alumni.login.models import CustomUser
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alumni.alumni.settings')
+django.setup()
 
 superuser = CustomUser.objects.create_superuser(email="student@yandex.ru",
                                                 first_name="Oleg",
