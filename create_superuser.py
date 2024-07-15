05b760de7a2c452bef97db0c0502fc5f1d67c243
Angelika2707 <anzelika272011@gmail.com>
2024-07-15 15:58:43 +0300

    create superuser

--- create_superuser.py
+++ create_superuser.py
@@ -8,3 +8,10 @@
 # User = get_user_model()
 #
 # User.objects.create_superuser(email="student@yandex.ru", password="admin", first_name="Ivan", last_name="Pupkin")
+from login.models import CustomUser
+
+superuser = CustomUser.objects.create_superuser(email="student@yandex.ru",
+                                                first_name="Oleg",
+                                                last_name="Milashkin",
+                                                password="admin")
+superuser.save()
