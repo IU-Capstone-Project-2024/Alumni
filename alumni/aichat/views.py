03500925be47606ccee74d325bdced6fba3f1e60
Chehmet <57629135+Chehmet@users.noreply.github.com>
2024-07-19 22:19:26 +0300

    Added russian embedings and removed nltk

--- alumni/aichat/views.py
+++ alumni/aichat/views.py
@@ -2,75 +2,57 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
-import nltk
 import numpy as np
 from sentence_transformers import SentenceTransformer, util
-from nltk.stem import PorterStemmer, WordNetLemmatizer
-from nltk.corpus import stopwords
-from nltk.tokenize import RegexpTokenizer
-from django.urls import path, include
-from django.views.generic import TemplateView
-from django.contrib import admin  # Import admin module
-
-# Download NLTK resources if not already downloaded
-nltk.download('punkt')
-nltk.download("stopwords")
-nltk.download("wordnet")
+from django.utils.safestring import mark_safe
 
 # Initialize Sentence Transformer model
-model = SentenceTransformer("sentence-transformers/distilbert-base-nli-stsb-mean-tokens")
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
 # Define webpages and their descriptions
 webpages = {
-    "Card request": "Card request. Card. Pass. Enter. Arrive. Go. University. Innopolis.",
-    "Hello! How can I help you?": "Hi. Hello. Good afternoon. Good evening. Good night. Hey.",
-    "Market": "Shop. Inno Shop. Merch. Merchandize. Gift.",
-    "My profile": "Log in. Profile. Authorization. User.",
-    "Mentorship": "Students. Teacher. Mentors. Mentorship. Support",
-    "Donation": "Donation. Money. Gifting. Gift. Supporting.",
-    "Events": "Meetups. Conferences. Events. Clubs",
-    "Homepage": "Homepage. Home. Menu.",
-    "Map": "Map. Alumnus. Cities. World"
+    "Card request": "Request a card for access. card. Pass. Enter. Arrive. Go. University. Innopolis. Запрос карты для доступа. карта. Пропуск. Войти. Прибыть. Идти. Университет. Иннополис.",
+    "Hello! How can I help you?": "Say hello. Hi. Hello. Good afternoon. Good evening. Good night. Hey. Скажите привет. Привет. Добрый день. Добрый вечер. Доброй ночи. Привет.",
+    "Market": "Shop for merchandise. Shop. Inno Shop. Merch. Merchandise. Gift. Магазин для покупок. Магазин. Ино Магазин. Товары. Подарок.",
+    "My profile": "Log in to your profile. Profile. Authorization. User. Войти в профиль. Профиль. Авторизация. Пользователь.",
+    "Mentorship": "Get support from mentors. Students. Teacher. Mentors. Mentorship. Support. Получить поддержку от наставников. Ментор. Студенты. Учитель. Наставники. Наставничество. Поддержка.",
+    "Donation": "Make a donation. Donation. Money. Gifting. Gift. Supporting. Сделать пожертвование. Пожертвование. Деньги. Подарок. Поддержка.",
+    "Events": "Learn about events. Meetups. Conferences. Events. Clubs. Узнать о событиях. Встречи. Конференции. События. Клубы.",
+    "Map": "Find locations on the map. Map. Alumnus. Cities. World. Найти местоположения на карте. Карта. Выпускник. Города. Мир."
 }
 
-# Convert webpages dictionary into lists for further processing
+
 webpage_keys = list(webpages.keys())
 webpage_descriptions = list(webpages.values())
 
-# Encode webpage descriptions using Sentence Transformer model
 webpage_embeddings = model.encode(webpage_descriptions, convert_to_tensor=True)
 
-# Function to preprocess text (lowercase and strip)
 def preprocess_text(text):
-    text = text.lower().strip()
-    return text
+    return text.lower().strip()
 
-# Function to check for inappropriate content
 def contains_inappropriate_content(text):
     inappropriate_words = ['sex', 'death', 'violence', 'drugs', 'abuse', 'depression']
-    for word in inappropriate_words:
-        if word in text.lower():
-            return True
-    return False
+    return any(word in text.lower() for word in inappropriate_words)
 
-# Function to create hyperlinks
 def create_hyperlinks(text):
     hyperlinks = {
-        'Card request': '<a href="http://130.193.43.164/card_request/">Card request</a>',
-        'Market': '<a href="http://130.193.43.164/market/">Market</a>',
-        'My profile': '<a href="http://130.193.43.164/my_profile/profile/">My profile</a>',
-        'Mentorship': '<a href="http://130.193.43.164/mentorship/">Mentorship</a>',
-        'Donation': '<a href="http://130.193.43.164/donation/">Donation</a>',
-        'Events': '<a href="https://130.193.43.164/events/">Events</a>',
-        'Homepage': '<a href="https://130.193.43.164/main/">Homepage</a>',
-        'Map': '<a href="https://130.193.43.164/map/">Map</a>'
+        'Card request': '<a href="http://130.193.43.164/card_request/" style="color: blue;" target="_blank">Card request</a>',
+        'Market': '<a href="http://130.193.43.164/market/" style="color: blue;" target="_blank">Market</a>',
+        'My profile': '<a href="http://130.193.43.164/my_profile/profile/" style="color: blue;" target="_blank">My profile</a>',
+        'Mentorship': '<a href="http://130.193.43.164/mentorship/" style="color: blue;" target="_blank">Mentorship</a>',
+        'Donation': '<a href="http://130.193.43.164/donation/" style="color: blue;" target="_blank">Donation</a>',
+        'Events': '<a href="https://130.193.43.164/events/" style="color: blue;" target="_blank">Events</a>',
+        # 'Homepage': '<a href="https://130.193.43.164/main/" style="color: blue;" target="_blank">Homepage</a>',
+        'Map': '<a href="https://130.193.43.164/map/" style="color: blue;" target="_blank">Map</a>'
     }
-    
-    for key, link in hyperlinks.items():
-        text = text.replace(key, link)
-    return text
 
-# CSRF exempted view for handling chat requests
+    for key, link in hyperlinks.items():
+        if key in text:
+            text = text.replace(key, link)
+            break      
+    return mark_safe(text)
+
+
 @csrf_exempt
 def chat(request):
     if request.method == 'POST':
@@ -81,7 +63,7 @@ def chat(request):
         search_query = preprocess_text(message)
 
         if contains_inappropriate_content(search_query):
-            response = "I didn't understand you. We have options:\n" + "\n".join(options)
+            response = "I didn't understand you. We have options:\n" + ", ".join(options)
         else:
             search_query_embedding = model.encode([search_query], convert_to_tensor=True)[0]
             cosine_similarities = util.pytorch_cos_sim(search_query_embedding, webpage_embeddings)[0].cpu().numpy()
@@ -89,12 +71,11 @@ def chat(request):
             most_similar_webpage = webpage_keys[most_similar_idx]
             score_text = cosine_similarities[most_similar_idx]
 
-            # Print the score to the console
             print(f"Query: {search_query}, Most Similar Webpage: {most_similar_webpage}, Score: {score_text}")
 
-            if score_text < 0.2:
+            if score_text < 0.25:
                 response = "I didn't understand you. We have options:\n" + "\n".join(options)
             else:
                 response = create_hyperlinks(most_similar_webpage.replace('_', ' '))
 
-        return JsonResponse({'response': response})
+        return JsonResponse({'response': str(response)})
