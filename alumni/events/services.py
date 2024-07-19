b8fd7d6ce6b6fd6e288cbb24229c525b0e91746a
WellNotWell <olesya.gredyushko4956@gmail.com>
2024-07-19 12:17:58 +0300

    del TODO

--- alumni/events/services.py
+++ alumni/events/services.py
@@ -2,15 +2,7 @@
 def get_recommended_events(events, interests):
     """
     Selects events suitable for the user based on their interests.
-
-    Args:
-    events (QuerySet): QuerySet of Events objects to filter from.
-    interests (list of str): List of interests of the user.
-
-    Returns:
-    list: Array of event IDs recommended for the user.
     """
-    # TODO: Write finding suitable events. Add parameters if needed.
     interests_set = set(interests)
     scored_events = []
 
