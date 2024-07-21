a4aeb0e3ccb003a79eb64fcd8bd1bb1503bfdca8
Chehmet <57629135+Chehmet@users.noreply.github.com>
2024-07-21 16:27:11 +0300

    Master chat without model

--- alumni/aichat/views.py
+++ alumni/aichat/views.py
@@ -1,36 +1,20 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
-import numpy as np
+import random
 from django.utils.safestring import mark_safe
-import requests
 
-# Define Hugging Face API details
-API_URL = "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2"
-headers = {"Authorization": "Bearer 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"}
-
-pages = ["Card request", "Hello! How can I help you?", "Market",
-         "My profile", "Mentorship", "Donation", "Events", "Map"]
-
-# Define webpages and their descriptions
-webpages = [
-    "Request a card for access. card. Pass. Enter. Arrive. Go. University. Innopolis. Запрос карты для доступа. карта. Пропуск. Войти. Прибыть. Идти. Университет. Иннополис.",
-    "Say hello. Hi. Hello. Good afternoon. Good evening. Good night. Hey. Скажите привет. Привет. Добрый день. Добрый вечер. Доброй ночи. Привет.",
-    "Shop for merchandise. Shop. Inno Shop. Merch. Merchandise. Gift. Магазин для покупок. Магазин. Ино Магазин. Товары. Подарок.",
-    "Log in to your profile. Profile. Authorization. User. Войти в профиль. Профиль. Авторизация. Пользователь.",
-    "Get support from mentors. Students. Teacher. Mentors. Mentorship. Support. Получить поддержку от наставников. Ментор. Студенты. Учитель. Наставники. Наставничество. Поддержка.",
-    "Make a donation. Donation. Money. Gifting. Gift. Supporting. Сделать пожертвование. Пожертвование. Деньги. Подарок. Поддержка.",
-    "Learn about events. Meetups. Conferences. Events. Clubs. Узнать о событиях. Встречи. Конференции. События. Клубы.",
-    "Find locations on the map. Map. Alumnus. Cities. World. Найти местоположения на карте. Карта. Выпускник. Города. Мир."
-]
-
-webpage_keys = pages
-webpage_descriptions = webpages
-
-# Function to call Hugging Face API
-def query(payload):
-    response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()
+# Define the pages and their hyperlinks
+pages = ["Card request", "Market", "My profile", "Mentorship", "Donation", "Events", "Map"]
+hyperlinks = {
+    'Card request': '<a href="http://130.193.43.164/card_request/" style="color: blue;" target="_blank">Card request</a>',
+    'Market': '<a href="http://130.193.43.164/market/" style="color: blue;" target="_blank">Market</a>',
+    'My profile': '<a href="http://130.193.43.164/my_profile/profile/" style="color: blue;" target="_blank">My profile</a>',
+    'Mentorship': '<a href="http://130.193.43.164/mentorship/" style="color: blue;" target="_blank">Mentorship</a>',
+    'Donation': '<a href="http://130.193.43.164/donation/" style="color: blue;" target="_blank">Donation</a>',
+    'Events': '<a href="https://130.193.43.164/events/" style="color: blue;" target="_blank">Events</a>',
+    'Map': '<a href="https://130.193.43.164/map/" style="color: blue;" target="_blank">Map</a>'
+}
 
 # Preprocess the text input
 def preprocess_text(text):
@@ -42,22 +26,8 @@ def contains_inappropriate_content(text):
     return any(word in text.lower() for word in inappropriate_words)
 
 # Create hyperlinks for responses
-def create_hyperlinks(text):
-    hyperlinks = {
-        'Card request': '<a href="http://130.193.43.164/card_request/" style="color: blue;" target="_blank">Card request</a>',
-        'Market': '<a href="http://130.193.43.164/market/" style="color: blue;" target="_blank">Market</a>',
-        'My profile': '<a href="http://130.193.43.164/my_profile/profile/" style="color: blue;" target="_blank">My profile</a>',
-        'Mentorship': '<a href="http://130.193.43.164/mentorship/" style="color: blue;" target="_blank">Mentorship</a>',
-        'Donation': '<a href="http://130.193.43.164/donation/" style="color: blue;" target="_blank">Donation</a>',
-        'Events': '<a href="https://130.193.43.164/events/" style="color: blue;" target="_blank">Events</a>',
-        'Map': '<a href="https://130.193.43.164/map/" style="color: blue;" target="_blank">Map</a>'
-    }
-
-    for key, link in hyperlinks.items():
-        if key in text:
-            text = text.replace(key, link)
-            break      
-    return mark_safe(text)
+def create_hyperlinks(page):
+    return mark_safe(hyperlinks.get(page, ''))
 
 @csrf_exempt
 def chat(request):
@@ -71,27 +41,8 @@ def chat(request):
         if contains_inappropriate_content(search_query):
             response = "I didn't understand you. We have options:\n" + ", ".join(options)
         else:
-            similarity_embedding = query({
-                "inputs": {
-                    "source_sentence": search_query,
-                    "sentences": webpages,
-                },
-            })
-
-            print("   ", similarity_embedding)
-
-            # Find the index of the highest similarity score
-            most_similar_idx = np.argmax(similarity_embedding)
-
-            # Get the most similar webpage description
-            most_similar_webpage = webpage_keys[most_similar_idx]
-
-
-            print(f"Query: {search_query}, Most Similar Webpage: {most_similar_webpage}")
-            # if score_text < 0.25:
-            #     response = "I didn't understand you. We have options:\n" + "\n".join(options)
-            # else:
-            #     response = create_hyperlinks(most_similar_webpage.replace('_', ' '))
-            response = create_hyperlinks(most_similar_webpage.replace('_', ' '))
+            # Select a random page
+            random_page = random.choice(pages)
+            response = create_hyperlinks(random_page)
 
         return JsonResponse({'response': str(response)})
